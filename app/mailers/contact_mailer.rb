fb43b3e1db8f488b4f4bdf7e2c5f76c8271b2672
Jelte Kroes <jelte.cruz@gmail.com>
2016-07-15 13:40:39 +0000

    Changed mailer default email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'jelte.cruz@gmail.com'
+  default to: 'nl.renovatie@gmail.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
