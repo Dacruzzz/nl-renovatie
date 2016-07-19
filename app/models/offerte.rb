5b9d70145834cb043e6e1c2b50c67a4566786ad4
Jelte Kroes <jelte.cruz@gmail.com>
2016-07-19 12:31:50 +0000

    Added active record validation

--- app/models/offerte.rb
+++ app/models/offerte.rb
@@ -4,10 +4,10 @@ class Offerte < ActiveRecord::Base
     validates :initials, presence: true
     validates :lastname, presence: true
     validates :adres, presence: true
-    validates :zipcode, presence: true
+    validates :zipcode, zipcode: { country_code: :nl }, presence: true
     validates :city, presence: true
-    validates :telnum, presence: true
-    validates :email, presence: true
+    validates_numericality_of :telnum, :only_integer => true, presence: true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, presence: true
     validates :subject, presence: true
     validates :message, presence: true
 end
