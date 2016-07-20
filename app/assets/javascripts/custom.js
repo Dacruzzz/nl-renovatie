7982cf37ac44445ee2cbd8e07d7ed898985f27e3
Jelte Kroes <jelte.cruz@gmail.com>
2016-07-20 23:53:23 +0000

    Minor changes

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,18 +1,20 @@
-$(document).ready(function(){
-    var pathname = window.location.pathname;
-    if (pathname === '/'){
-        $(".rootbutton").css({'color': '#337ab7', 'background-color': 'white'});
-    }
-    else if (pathname === '/overons'){
-        $(".overonsbutton").css({'color': '#337ab7', 'background-color': 'white'});
-    }
-    else if (pathname === '/projecten'){
-        $(".projectenbutton").css({'color': '#337ab7', 'background-color': 'white'});
-    }
-    else if (pathname === '/offertes/new'){
-        $(".offertebutton").css({'color': '#337ab7', 'background-color': 'white'});
+if (screen.width >= 780){
+    $(document).ready(function(){
+        var pathname = window.location.pathname;
+        if (pathname === '/'){
+            $(".rootbutton").css({'color': '#337ab7', 'background-color': 'white'});
         }
-    else if (pathname === '/contacts/new'){
-        $(".contactbutton").css({'color': '#337ab7', 'background-color': 'white'});
-    }
-});
+        else if (pathname === '/overons'){
+            $(".overonsbutton").css({'color': '#337ab7', 'background-color': 'white'});
+        }
+        else if (pathname === '/projecten'){
+            $(".projectenbutton").css({'color': '#337ab7', 'background-color': 'white'});
+        }
+        else if (pathname === '/offertes/new'){
+            $(".offertebutton").css({'color': '#337ab7', 'background-color': 'white'});
+            }
+        else if (pathname === '/contacts/new'){
+            $(".contactbutton").css({'color': '#337ab7', 'background-color': 'white'});
+        }
+    });
+}
