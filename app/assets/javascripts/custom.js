8fc1538701b693250c8e39d58fb9a6674e2ae581
Jelte Kroes <jelte.cruz@gmail.com>
2016-08-02 18:54:20 +0000

    Added JS file validation

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,4 @@
+//NAVBAR BUTTON COLORS ONLY FOR BIG SCREENS
 if (screen.width >= 780){
     $(document).ready(function(){
         var pathname = window.location.pathname;
@@ -18,10 +19,40 @@ if (screen.width >= 780){
         }
     });
 }
+//END OF BUTTON COLORS
 
+//SCREEN ADJUSTMENT FOR BIG SCREENS
 if (screen.width >= 1400){
     $(document).ready(function(){
         var setxlh = screen.height*0.9;
         $("#panelprojecten").css({'max-height':setxlh})
     });
 }
+//END OF ADJUSTMENT
+
+//CLIENT SIDE FILE VALIDATION
+function validateFiles(inputFile) {
+  var maxExceededMessage = "Dit bestand overschrijdt de maximum grootte van 5 mb";
+  var extErrorMessage = "Alleen afbeeldingen van het formaat: .jpg, .jpeg, .gif or .png zijn toegestaan";
+  var allowedExtension = ["jpg", "jpeg", "gif", "png"];
+
+  var extName;
+  var maxFileSize = $(inputFile).data('max-file-size');
+  var sizeExceeded = false;
+  var extError = false;
+
+  $.each(inputFile.files, function() {
+    if (this.size && maxFileSize && this.size > parseInt(maxFileSize)) {sizeExceeded=true;};
+    extName = this.name.split('.').pop();
+    if ($.inArray(extName, allowedExtension) == -1) {extError=true;};
+  });
+  if (sizeExceeded) {
+    window.alert(maxExceededMessage);
+    $(inputFile).val('');
+  };
+
+  if (extError) {
+    window.alert(extErrorMessage);
+    $(inputFile).val('');
+  };
+}//END OF CLIENT SIDE VALIDATION
